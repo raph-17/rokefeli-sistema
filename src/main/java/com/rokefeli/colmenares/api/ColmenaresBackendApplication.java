651e51ef4ecb52a24bcc1e28a6293aeedd1ab848
raph-17 <ricardoplazaxdd@gmail.com>
2025-11-04 13:13:13 -0500

    creacion del backend del proyecto

---
+++ src/main/java/com/rokefeli/colmenares/api/ColmenaresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rokefeli.colmenares.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColmenaresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColmenaresBackendApplication.class, args);
+	}
+
+}
