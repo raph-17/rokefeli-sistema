f9c5591f3000bc5fd45264256ddf0ff8485bfb5f
raph-17 <ricardoplazaxdd@gmail.com>
2025-11-09 21:02:28 -0500

    retirado import innecesario para productoupdate

--- src/main/java/com/rokefeli/colmenares/api/dto/update/ProductoUpdateDTO.java
+++ src/main/java/com/rokefeli/colmenares/api/dto/update/ProductoUpdateDTO.java
@@ -4,8 +4,6 @@ import com.rokefeli.colmenares.api.dto.create.ProductoCreateDTO;
 import com.rokefeli.colmenares.api.entity.enums.EstadoProducto;
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class ProductoUpdateDTO extends ProductoCreateDTO {
     private EstadoProducto estado;
