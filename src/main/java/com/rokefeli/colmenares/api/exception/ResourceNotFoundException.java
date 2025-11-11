fb723f198c7ce5223e4d90de25550c9c68aeb3a0
leylayui7 <leylayui7@gmail.com>
2025-11-11 03:37:40 -0500

    Delete src/main/java/com/rokefeli/colmenares/api/exception directory

--- src/main/java/com/rokefeli/colmenares/api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rokefeli.colmenares.api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource, Object id) {
-        super(String.format("%s with id '%s' not found", resource, id));
-    }
-}
