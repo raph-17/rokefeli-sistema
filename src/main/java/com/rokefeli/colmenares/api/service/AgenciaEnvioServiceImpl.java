fa9edd3212cf68a0fa8a4c52f3de05c0befe5bd5
leylayui7 <leylayui7@gmail.com>
2025-11-11 04:04:57 -0500

    Update AgenciaEnvioServiceImpl.java

--- src/main/java/com/rokefeli/colmenares/api/service/AgenciaEnvioServiceImpl.java
+++ src/main/java/com/rokefeli/colmenares/api/service/AgenciaEnvioServiceImpl.java
@@ -1,7 +1,7 @@
 package com.rokefeli.colmenares.api.service;
 
 import com.rokefeli.colmenares.api.entity.AgenciaEnvio;
-import com.rokefeli.colmenares.api.exception.ResourceNotFoundException;
+import com.rokefeli.colmenares.exception.ResourceNotFoundException;
 import com.rokefeli.colmenares.api.repository.AgenciaEnvioRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
