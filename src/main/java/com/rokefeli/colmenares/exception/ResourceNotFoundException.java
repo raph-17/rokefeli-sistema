8f731bbb1de375137284a4ee2a38f3bc3838b892
leylayui7 <leylayui7@gmail.com>
2025-11-11 03:38:10 -0500

    Add files via upload

---
+++ src/main/java/com/rokefeli/colmenares/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.rokefeli.colmenares.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(String.format("%s with id '%s' not found", resource, id));
+    }
+}
