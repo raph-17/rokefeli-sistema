d3b7ede561bc2d6edd089e07f4bb3220d54aa645
raph-17 <ricardoplazaxdd@gmail.com>
2025-11-11 18:10:21 -0500

    DetalleVentaMapper

---
+++ src/main/java/com/rokefeli/colmenares/api/mapper/DetalleVentaMapper.java
@@ -0,0 +1,18 @@
+package com.rokefeli.colmenares.api.mapper;
+
+import com.rokefeli.colmenares.api.dto.create.DetalleVentaCreateDTO;
+import com.rokefeli.colmenares.api.dto.response.DetalleVentaResponseDTO;
+import com.rokefeli.colmenares.api.entity.DetalleVenta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DetalleVentaMapper {
+
+    DetalleVenta toEntity(DetalleVentaCreateDTO dto);
+
+    @Mapping(source = "producto.id", target = "idProducto")
+    @Mapping(source = "producto.nombre", target = "nombreProducto")
+    DetalleVentaResponseDTO toResponseDTO(DetalleVenta entity);
+    
+}
