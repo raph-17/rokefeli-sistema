9d063c6dc48a53fa7aa5ab5489b5dd40fb96d678
raph-17 <ricardoplazaxdd@gmail.com>
2025-11-11 19:52:05 -0500

    actualizacion de UsuarioMapper y eliminacion de import en DetalleVentaCreateDTO

--- src/main/java/com/rokefeli/colmenares/api/dto/create/DetalleVentaCreateDTO.java
+++ src/main/java/com/rokefeli/colmenares/api/dto/create/DetalleVentaCreateDTO.java
@@ -2,7 +2,6 @@ package com.rokefeli.colmenares.api.dto.create;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import java.math.BigDecimal;
 
 @Data
 public class DetalleVentaCreateDTO {
--- src/main/java/com/rokefeli/colmenares/api/mapper/UsuarioMapper.java
+++ src/main/java/com/rokefeli/colmenares/api/mapper/UsuarioMapper.java
@@ -1,7 +1,9 @@
 package com.rokefeli.colmenares.api.mapper;
 
+import com.rokefeli.colmenares.api.dto.create.AdminCreateDTO;
 import com.rokefeli.colmenares.api.dto.create.UsuarioCreateDTO;
 import com.rokefeli.colmenares.api.dto.response.UsuarioResponseDTO;
+import com.rokefeli.colmenares.api.dto.update.AdminUpdateDTO;
 import com.rokefeli.colmenares.api.dto.update.UsuarioUpdateDTO;
 import com.rokefeli.colmenares.api.entity.Usuario;
 import org.mapstruct.*;
@@ -11,8 +13,14 @@ public interface UsuarioMapper {
 
     Usuario toEntity(UsuarioCreateDTO dto);
 
+    Usuario toEntityFromAdminCreateDTO(AdminCreateDTO dto);
+
     UsuarioResponseDTO toResponseDTO(Usuario entity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDTO(UsuarioUpdateDTO dto, @MappingTarget Usuario entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromAdminUpdateDTO(AdminUpdateDTO dto, @MappingTarget Usuario entity);
+
 }
